e8d9f33ae4408130c2700a3b606784cfd3b9ef19
arielchernis <44745531+arielchernis@users.noreply.github.com>
2022-07-30 16:41:35 +0300

    Reg looks update

--- FrontEnd/screens/Register_screen.tsx
+++ FrontEnd/screens/Register_screen.tsx
@@ -1,12 +1,43 @@
 import React,{FC} from "react"
 import {View, Text} from "react-native"
+import { Button,Icon,Input } from '@rneui/themed';
 
 
 const Login: FC<{ navigation: any, route: any }> = ({ navigation, route }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Register_Screen</Text>
-    </View>
+
+            <Icon
+                reverse
+                name="person-circle-outline"
+                type='ionicon'
+            />
+
+            <Input
+                placeholder='Email'
+                leftIcon={{  name: 'email' }}
+                />
+            <Input
+                placeholder='Password'
+                leftIcon={{ type :'ionicon', name: 'key-outline' }}
+            />
+            <Button
+                title="REGISTER"
+                buttonStyle={{
+                    backgroundColor: 'black',
+                    borderWidth: 2,
+                    borderColor: 'white',
+                    borderRadius: 30,
+                }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}
+                titleStyle={{ fontWeight: 'bold' }}
+            />
+
+        </View>
 )
 }
 
